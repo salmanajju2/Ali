8baacf3802cf5a19179d770beece7706b52147f2
Ali Enterprises <abdulkadir706065@gmail.com>
2026-08-13 08:35:08 +0000

    Remove all legacy D1 and Cloudflare Worker files permanently

--- scratch_query.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const dump = JSON.parse(fs.readFileSync('d1_dump.json', 'utf8'));
-const results = dump.transactions.filter(tx => tx.bank && tx.bank.includes('SPICEMONEY'));
-console.log(JSON.stringify(results.slice(0, 10), null, 2));
